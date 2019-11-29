a2fc9e3f7f6ecf9718558248cc098b7207ddac06
scathajs <58314529+scathajs@users.noreply.github.com>
2019-11-28 19:13:58 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));
+
+app.get("/", function (req, res) {
+    res.send('Welcome to timestamp server')
+});
+
+app.get('/api/timestamp/', function(req, res) {
+    let date = new Date();
+    res.json({
+        unix: date.getTime(),
+        utc: date.toUTCString()
+    });
+});
+
+app.get('/api/timestamp/:date', function(req, res) {
+    let date = new Date(req.params.date);
+    if (isNaN(date.getTime())) {
+        res.json({error: 'Invalid Date'});
+        return;
+    }
+    res.json({
+        unix: date.getTime(),
+        utc: date.toUTCString()
+    });
+});
+
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
